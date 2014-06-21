ed7b054aa7714f89f8b2bf8268d8a02c05e9655b
Joakim Uddholm <joakim@uddholm.com>
2014-06-21 19:51:23 +0200

    MD5 length extension attack.

---
+++ md5/length-extension.py
@@ -0,0 +1,42 @@
+import md5alt
+import struct
+
+def hexdump(s):
+	for b in xrange(0, len(s), 16):
+		lin = [c for c in s[b : b + 16]]
+		#~ if sum([ord(l) for l in lin]) == 0:
+			#~ continue
+		hxdat = ' '.join('%02X' % ord(c) for c in lin)
+		pdat = ''.join((c if 32 <= ord(c) <= 126 else '.' )for c in lin)
+		print('  %04x: %-48s %s' % (b, hxdat, pdat))
+	print
+
+secret = b"secret"
+original = b"data"
+append = b"append"
+
+def pad(s):
+	padlen = 64 - ((len(s) + 8) % 64)
+	bit_len = 8*len(s)
+	if(padlen < 64):
+		s += '\x80' + '\000' * (padlen - 1)
+	return s + struct.pack('<q', bit_len)
+
+val = md5alt.new(secret+original)
+print "Original payload:", val.hexdigest()
+
+payload = pad(secret+original)+append
+hexdump(payload)
+
+legit = md5alt.new(payload)
+print "Legit digest:", legit.hexdigest()
+
+not_legit = md5alt.new("A"*64)
+not_legit.A, not_legit.B, not_legit.C, not_legit.D = md5alt._bytelist2long(val.digest())
+not_legit.update(append)
+print "Illicit digest:", not_legit.hexdigest()
+
+if legit.hexdigest() == not_legit.hexdigest():
+	print "Success!"
+else:
+	print "Fail!"
